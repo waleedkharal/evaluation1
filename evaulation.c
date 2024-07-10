f1a466b7fea64a9d4ce7c0a59600657bbf3a9947
waleedkharal <158275816+waleedkharal@users.noreply.github.com>
2024-07-10 14:45:29 +0500

    Add files via upload

---
+++ evaulation.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+int  divison_algorithm(int A, int n ,  int Q, int M){
+
+	int i;
+	while(n!==0){
+
+	        A=A<<1;
+	        Q=q<<1;
+	        A=A-M;
+	        if(A==0){//it is hard to get to know the msb of the A
+			Q[0]=1;
+	}
+                 else:
+	         Q[0]=0;
+	 A=A;
+	 n=n -1;
+
+
+}};
+
+
+int main(){
+	
+	
+	
+	register int A=0;
+	register int n=sizeof(Q) ;
+	register int Q;
+	register int M ;
+	printf("Enter value of A: ");
+        scanf("%d", &n);
+	printf("Enter value of Q: ");
+        scanf("%d", &Q);
+	printf("Enter value of M: ");
+        scanf("%d", &M);
+
+
+
+
+
+}
